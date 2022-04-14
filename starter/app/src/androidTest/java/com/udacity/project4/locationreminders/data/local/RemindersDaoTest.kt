5387691397d4910deee9a3a8c9fe2fd142cd27a8
vivianazevedopereira <vivispereira.21@gmail.com>
2022-04-14 10:27:54 +1000

    Adding unit test for RemindersDaoTest and RemindersListViewModelTest

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -1,30 +1,85 @@
 package com.udacity.project4.locationreminders.data.local
 
+import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
-import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-//Unit test the DAO
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var remindersDatabase: RemindersDatabase
+
+    @Before
+    fun initDatabase() {
+        remindersDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        )
+            .allowMainThreadQueries().build()
+    }
+
+    @Test
+    fun insertReminder() = runBlocking {
+        // get a reminder
+        val reminder = ReminderDTO(
+            "Title",
+            "Description",
+            "Location",
+            39.0,
+            41.6
+        )
+        remindersDatabase.reminderDao().saveReminder(reminder)
+
+        // insert the reminder by id from the database
+        val loaded = remindersDatabase.reminderDao().getReminderById(reminder.id)
+        // loaded data contains the expected values
+        assertThat(loaded as ReminderDTO, notNullValue())
+        assertThat(loaded.id, `is`(reminder.id))
+        assertThat(loaded.title, `is`(reminder.title))
+        assertThat(loaded.description, `is`(reminder.description))
+        assertThat(loaded.latitude, `is`(reminder.latitude))
+        assertThat(loaded.longitude, `is`(reminder.longitude))
+        assertThat(loaded.location, `is`(reminder.location))
+    }
+
+    @Test
+    fun noReminderForDeleted() = runBlocking {
+        // get a reminder
+        val reminder = ReminderDTO(
+            "Title",
+            "Description",
+            "Location",
+            29.0,
+            30.4)
+
+        remindersDatabase.reminderDao().saveReminder(reminder)
+        remindersDatabase.reminderDao().deleteAllReminders()
+
+        val id = reminder.id
+        val result = remindersDatabase.reminderDao().getReminderById(id)
+
+        assertThat(result, `is`(nullValue()))
+    }
+
+    @After
+    fun closeDatabase() = remindersDatabase.close()
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,87 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
+    // subject under test
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    // fake repo to be injected into viewmodel
+    private lateinit var fakeDataSource: FakeDataSource
+
+    // main coroutines dispatcher for unit testing
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupRepository() {
+        stopKoin()
+
+        fakeDataSource = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(
+            getApplicationContext(),
+            fakeDataSource
+        )
+    }
+
+    @Test
+    fun loadingWithNoData() = runBlockingTest {
+        fakeDataSource.shouldReturnError = true
+        remindersListViewModel.loadReminders()
+
+        assertThat(
+            remindersListViewModel.showSnackBar.getOrAwaitValue(),
+            `is`("Reminders not found")
+        )
+    }
+
+    @Test
+    fun loadingWithData() = runBlocking {
+        // get a reminder
+        val reminder = ReminderDTO(
+            "Title",
+            "Description",
+            "Location",
+            26.19,
+            32.25
+        )
+
+        fakeDataSource.saveReminder(reminder)
+
+        mainCoroutineRule.pauseDispatcher()
+
+        remindersListViewModel.loadReminders()
+
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(true))
+
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(false))
+    }
 }
