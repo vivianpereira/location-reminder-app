1d0dd6cdcd78e3a2d46cdcdc444fc5d8d3c6efb2
vivianazevedopereira <vivispereira.21@gmail.com>
2022-05-04 15:56:32 +1000

    Updating LocalDB

--- starter/app/src/main/java/com/udacity/project4/locationreminders/data/local/LocalDB.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/data/local/LocalDB.kt
@@ -1,7 +1,9 @@
 package com.udacity.project4.locationreminders.data.local
 
+import android.app.Application
 import android.content.Context
 import androidx.room.Room
+import org.koin.core.module.Module
 
 
 /**
@@ -12,7 +14,7 @@ object LocalDB {
     /**
      * static method that creates a reminder class and returns the DAO of the reminder
      */
-    fun createRemindersDao(context: Context): RemindersDao {
+    fun createRemindersDao(context: Application): RemindersDao {
         return Room.databaseBuilder(
             context.applicationContext,
             RemindersDatabase::class.java, "locationReminders.db"
