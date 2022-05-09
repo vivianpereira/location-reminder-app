d7e56e53be4fea44091ce2ef74834edf5172fa2a
vivianazevedopereira <vivispereira.21@gmail.com>
2022-05-09 15:10:18 +1000

    Implementing Espresso test para toast message

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.*
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -22,8 +25,10 @@ import com.udacity.project4.util.DataBindingIdlingResource
 import com.udacity.project4.util.monitorActivity
 import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
 import org.junit.Before
@@ -85,21 +90,53 @@ class RemindersActivityTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun click_add_a_new_reminder() = runBlockingTest {
+    fun click_add_a_new_reminder_error() = runBlockingTest {
 
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
-        val activity = getActivity(activityScenario)
 
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
         onView(withId(R.id.addReminderFAB)).perform(click())
         onView(withId(R.id.reminderTitle)).check(matches(isDisplayed()))
         onView(withId(R.id.reminderDescription)).check(matches(isDisplayed()))
         onView(withId(R.id.selectLocation)).check(matches(isDisplayed()))
         onView(withId(R.id.saveReminder)).perform(click())
-        onView(withText(R.string.err_enter_title)).inRoot(withDecorView(
-            `is`(getActivity(activityScenario)?.window?.decorView)
-        )).check(matches(isDisplayed()))
+        onView(withText(R.string.err_enter_title)).inRoot(
+            withDecorView(
+                `is`(getActivity(activityScenario)?.window?.decorView)
+            )
+        ).check(matches(isDisplayed()))
 
+        activityScenario.close()
+    }
+
+    @Test
+    fun click_add_a_new_reminder_success() = runBlockingTest {
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(ViewActions.replaceText("Title"))
+        onView(withId(R.id.reminderDescription)).perform(ViewActions.replaceText("Description"))
+        closeSoftKeyboard()
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withContentDescription("Google Map")).perform(click())
+        onView(withId(R.id.save_button)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
+        Thread.sleep(2000)
+        onView(withText(R.string.reminder_saved))
+            .inRoot(
+                RootMatchers.withDecorView(
+                    CoreMatchers.not(
+                        `is`(
+                            getActivity(
+                                activityScenario
+                            )!!.window.decorView
+                        )
+                    )
+                )
+            )
         activityScenario.close()
     }
 
